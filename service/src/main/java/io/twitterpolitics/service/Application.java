7901e9b9c6da572df4f59ad1c93f53c480d2489d
esuau <evan.suau@etu.u-pec.fr>
2018-02-20 15:09:56 +0100

    Set packaging app to deployable WAR file

--- service/src/main/java/io/twitterpolitics/service/Application.java
+++ service/src/main/java/io/twitterpolitics/service/Application.java
@@ -2,6 +2,8 @@ package io.twitterpolitics.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +14,12 @@ import org.springframework.context.annotation.Configuration;
         "io.twitterpolitics.repository",
         "io.twitterpolitics.service.controller"
 })
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
