ce6b47b93f3d2ab927ee4e37d9d203f3cc3ca18c
esuau <evan.suau@etu.u-pec.fr>
2018-02-25 17:23:37 +0100

    Properly set package scanning

--- service/src/main/java/io/twitterpolitics/service/Application.java
+++ service/src/main/java/io/twitterpolitics/service/Application.java
@@ -2,18 +2,22 @@ package io.twitterpolitics.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan(basePackages = {
-        "io.twitterpolitics.entity",
-        "io.twitterpolitics.repository",
-        "io.twitterpolitics.service.controller"
-})
+@EnableJpaRepositories("io.twitterpolitics.repository")
+@ComponentScan({"io.twitterpolitics.service.controller", "io.twitterpolitics.service.service"})
+@EntityScan("io.twitterpolitics.entity")
+@EnableTransactionManagement
+@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
     @Override
